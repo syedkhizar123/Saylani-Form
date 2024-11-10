bf483aeeb283fa5f02700722764609232f5d2d62
syedkhizar123 <khizarrehman2007@gmail.com>
2024-11-10 03:12:38 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+function submit(){
+    var country = document.getElementById("country")
+    var city = document.getElementById("city")
+    var course = document.getElementById("course")
+    var proficiency = document.getElementById("proficiency")
+    var fullname = document.getElementById("fullname")
+    var fathername = document.getElementById("fathername")
+    var email = document.getElementById("email")
+    var phone = document.getElementById("phone")
+    var cnic = document.getElementById("cnic")
+    var fathers_cnic = document.getElementById("father'scnic")
+    var date_of_birth = document.getElementById("DateOfBirth")
+    var gender = document.getElementById("Gender")
+    var address = document.getElementById("address")
+    var qualification = document.getElementById("qualification")
+    var laptop = document.getElementById("Laptop")
+
+
+
+
+    console.log(`Country: ${country.value}`)
+    console.log(`City: ${city.value}`)
+    console.log(`Course: ${course.value}`)
+    console.log(`Proficiency: ${proficiency.value}`)
+    console.log(`Full Name: ${fullname.value}`)
+    console.log(`Father Name: ${fathername.value}`)
+    console.log(`Email: ${email.value}`)
+    console.log(`Phone: ${phone.value}`)
+    console.log(`CNIC: ${cnic.value}`)
+    console.log(`Father's CNIC: ${fathers_cnic.value}`)
+    console.log(`Date of Birth: ${date_of_birth.value}`)
+    console.log(`Gender: ${gender.value}`)
+    console.log(`Address: ${address.value}`)
+    console.log(`Qualification: ${qualification.value}`)
+    console.log(`Laptop: ${laptop.value}`)
+}
+
+
